d0e40d9171db3c7c31937dc827ec31e7b28e48f8
Carlos Filipe <thecarlosfilipe@gmail.com>
2025-06-22 01:31:22 -0300

    feat: add marketing layout and home page with introductory content
    feat: create 'Sobre' page detailing platform benefits and features
    
    feat: implement form item component for job application form
    
    feat: establish vagas layout for job listings section
    
    feat: develop job item component to display individual job details

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,23 +3,20 @@ import { buttonVariants } from "../components/ui/button";
 
 export default function NotFound() {
   return (
-    <main className="py-10 text-center">
-      <h1 className="font-display text-5xl font-bold text-gray-700">
+    <main className="mx-auto max-w-5xl py-10 text-center">
+      <h1 className="font-display text-4xl font-bold text-gray-700">
         Nada encontrado aqui...
       </h1>
       <img
-        className="mx-auto mt-10 w-lg"
-        src="astronaut.png"
+        className="mx-auto mt-10 w-xs"
+        src="/astronaut.png"
         alt="Imagem de um astronauta perdido no espaço."
       />
       <p className="text-3xl font-light text-gray-500">
         Não foi possível encontrar a página solicitada.
       </p>
       <a
-        className={cn(
-          buttonVariants({ variant: "default" }),
-          "mt-12 px-6 py-6 text-lg",
-        )}
+        className={cn(buttonVariants({ variant: "default" }), "mt-6")}
         href="/"
       >
         Voltar à Home
